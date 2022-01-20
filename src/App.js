98bd7d3af35faf9e616bcad3198ed0c89c1931c6
PaolaSantolloV <97254325+PaolaSantolloV@users.noreply.github.com>
2022-01-20 16:18:13 -0600

    test: add test of picture of the day

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>NASA Picture of the day</h1>
+        <iframe title="picture-of-the-day" src="http://www.google.com" />
+        <h2>title picture</h2>
+        <p title="description">description picture</p>
       </header>
     </div>
   );
 }
-
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
-import React from "react";
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import React from "react";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<HomePage>", () => {
+  test("should render the title app correctly", () => {
+    render(<App />);
+    const titleElement = screen.getByText("NASA Picture of the day");
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test("should render the image of picture of the day correctly", () => {
+    render(<App />);
+    const picture = screen.getByTitle("picture-of-the-day");
+    expect(picture).toBeInTheDocument();
+  });
+
+  test("should render the title of picture of the day correctly", () => {
+    render(<App />);
+    const titlePicture = screen.getByRole("heading", { level: 2 });
+    expect(titlePicture).toBeInTheDocument();
+  });
+
+  test("should render the description of picture of the day correctly", () => {
+    render(<App />);
+    const descriptionPicture = screen.getByTitle("description");
+    expect(descriptionPicture).toBeInTheDocument();
+  });
 });
